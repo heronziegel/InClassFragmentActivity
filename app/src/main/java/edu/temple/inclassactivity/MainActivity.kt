065dea57a8dc81740d06a17c573dba9a23960bce
heronziegel <tuo12440@temple.edu>
2023-10-24 15:19:57 -0400

    Updates made in class 10/24

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +18,17 @@ class MainActivity : AppCompatActivity() {
         // Attach an instance of ImageDisplayFragment using factory method
         val fragment1 = ImageDisplayFragment.newInstance(imageArray)
 
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView,fragment1).commit()
+//        if(supportFragmentManager.findFragmentById(R.id.fragmentContainerView) !is ImageDisplayFragment)
+//            supportFragmentManager
+//                .beginTransaction()
+//                .add(R.id.fragmentContainerView,fragment1)
+//                .addToBackStack(null)
+//                .setReorderingAllowed(true)
+//                .commit()
 
     }
+
+//    override fun imageSelected(itemId: Int){
+//        Toast.makeText(this,"You selected $itemId", Toast.LENGTH_SHORT).show()
+//    }
 }
